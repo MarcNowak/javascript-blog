2497e9cd1c801cea35dd973e96a57b5a530dbe6e
Marcin Nowak <maarcin.nowaak@gmail.com>
2021-08-15 08:56:10 +0200

    Add remove post active

--- js/script.js
+++ js/script.js
@@ -1,42 +1,39 @@
-debugger;
-
 // document.getElementById('test-button').addEventListener('click', function(){
 //     const links = document.querySelectorAll('.titles a');
 //     console.log('links:', links);
 //   });
 
-const titleClickHandler = function (event) {
+const titleClickHandler = function(event){
     console.log('Link was clicked!');
     console.log(event);
-
+  
     /* remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
-    for (let activeLink of activeLinks) {
-        activeLink.classList.remove('active');
+    for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
     }
-
+  
     /* add class 'active' to the clicked link */
-
+  
     /* remove class 'active' from all articles */
 
-    const activeArticles = document.querySelectorAll('.post .active');
+    const activeArticles = document.querySelectorAll('.post.active');
 
-    for (let activeArticle of activeArticles) {
-        activeArticle.classList.remove('active');
+    for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
     }
-
+  
     /* get 'href' attribute from the clicked link */
-
+  
     /* find the correct article using the selector (value of 'href' attribute) */
-
+  
     /* add class 'active' to the correct article */
-
-
-    const links = document.querySelectorAll('.titles a');
-
-    for (let link of links) {
-        link.addEventListener('click', titleClickHandler);
-    }
-}
+  }
+  
+  const links = document.querySelectorAll('.titles a');
+  
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
