6f53c30bab1a3c075d194e50cb3734a0b67fd14b
Marcin Nowak <maarcin.nowaak@gmail.com>
2021-08-15 10:49:10 +0200

    Add Event Handler - Final

--- js/script.js
+++ js/script.js
@@ -3,40 +3,50 @@
 //     console.log('links:', links);
 //   });
 
-const titleClickHandler = function(event){
+const titleClickHandler = function (event) {
+    event.preventDefault();
     const clickedElement = this;
-    clickedElement.classList.add('active');
+    
     console.log('Link was clicked!');
-      
+
     /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
-    for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
     }
-  
-    /* [IN PROGRESS] add class 'active' to the clicked link */
 
+    /* [DONE] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
     console.log('clickedElement:', clickedElement);
-  
+
     /* [DONE} remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.post.active');
 
-    for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
     }
-  
+
     /* get 'href' attribute from the clicked link */
-  
+
+    const articleSelector = clickedElement.getAttribute("href");
+    console.log(articleSelector);
+
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+
     /* add class 'active' to the correct article */
-  }
-  
-  const links = document.querySelectorAll('.titles a');
-  
-  for(let link of links){
+
+    const clickedArticle = document.querySelector(articleSelector);
+    clickedArticle.classList.add('active');
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for (let link of links) {
     link.addEventListener('click', titleClickHandler);
-  }
+}
