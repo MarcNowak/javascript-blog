2b50293c7720814c7d62c63d9269732129eb09bd
Marcin Nowak <maarcin.nowaak@gmail.com>
2021-08-16 11:41:18 +0200

    Final 2

--- js/script.js
+++ js/script.js
@@ -3,17 +3,17 @@
 //     console.log('links:', links);
 //   });
 {
-const titleClickHandler = function (event) {
+  const titleClickHandler = function (event) {
     event.preventDefault();
     const clickedElement = this;
-    
+
     console.log('Link was clicked!');
 
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for (let activeLink of activeLinks) {
-        activeLink.classList.remove('active');
+      activeLink.classList.remove('active');
     }
 
     /* [DONE] add class 'active' to the clicked link */
@@ -24,7 +24,7 @@ const titleClickHandler = function (event) {
     const activeArticles = document.querySelectorAll('.post.active');
 
     for (let activeArticle of activeArticles) {
-        activeArticle.classList.remove('active');
+      activeArticle.classList.remove('active');
     }
 
     /* [DONE] get 'href' attribute from the clicked link */
@@ -38,53 +38,53 @@ const titleClickHandler = function (event) {
     /* [DONE] add class 'active' to the correct article */
     const clickedArticle = document.querySelector(articleSelector);
     clickedArticle.classList.add('active');
-}
-
-// Generate Title Links
-
-const optArticleSelector = '.post';
-const optTitleSelector = '.post-title';
-const optTitleListSelector = '.titles';
-
-function generateTitleLinks(){
-
-  /* [DONE] remove contents of titleList */
-  const titleList = document.querySelector(optTitleListSelector);
-  titleList.innerHTML = '';
-  console.log(titleList);
-
-  let html = '';
-
-  /* for each article */
-  const articles = document.querySelectorAll(optArticleSelector);
-  for(let article of articles){
-
-    /* get the article id */
-    const articleId = article.getAttribute('id');
-    console.log(articleId);
-
-    /* find the title element */
-
-    /* get the title from the title element */
-    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-    console.log(articleTitle);
-
-    /* create HTML of the link */
-    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-    console.log(linkHTML);
-
-    /* insert link into titleList */
-    titleList.innerHTML = titleList.innerHTML + linkHTML;
-    // linkHTML.insertAdjacentHTML('beforeend', linkHTML);
-    
   }
-  const links = document.querySelectorAll('.titles a');
-console.log(links);
 
-for (let link of links) {
-    link.addEventListener('click', titleClickHandler);
+  // Generate Title Links
+
+  const optArticleSelector = '.post';
+  const optTitleSelector = '.post-title';
+  const optTitleListSelector = '.titles';
+
+  function generateTitleLinks() {
+
+    /* [DONE] remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+    console.log(titleList);
+
+    let html = '';
+
+    /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector);
+    for (let article of articles) {
+
+      /* get the article id */
+      const articleId = article.getAttribute('id');
+      console.log(articleId);
+
+      /* find the title element */
+
+      /* get the title from the title element */
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+      console.log(articleTitle);
+
+      /* create HTML of the link */
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      console.log(linkHTML);
+
+      /* insert link into titleList */
+      titleList.innerHTML = titleList.innerHTML + linkHTML;
+      // linkHTML.insertAdjacentHTML('beforeend', linkHTML);
+
+    }
+    const links = document.querySelectorAll('.titles a');
+    console.log(links);
+
+    for (let link of links) {
+      link.addEventListener('click', titleClickHandler);
+    }
   }
-}
 
-generateTitleLinks();
+  generateTitleLinks();
 }
